701f54dfbfbd155aebb8dac2deb109f998ffff75
ocean-p <duongkodoiqua@gmail.com>
2021-04-10 15:29:45 +0700

    Upload Source

---
+++ Movie Management/SE140501/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace SE140501
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtgenre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtlength = new System.Windows.Forms.TextBox();
+            this.txtyear = new System.Windows.Forms.TextBox();
+            this.txttitle = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnew = new System.Windows.Forms.Button();
+            this.btadd = new System.Windows.Forms.Button();
+            this.btupdate = new System.Windows.Forms.Button();
+            this.btdelete = new System.Windows.Forms.Button();
+            this.movielist = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.btexit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Movie Management";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtgenre);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtlength);
+            this.groupBox1.Controls.Add(this.txtyear);
+            this.groupBox1.Controls.Add(this.txttitle);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(52, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(285, 222);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Movie Detail";
+            // 
+            // txtgenre
+            // 
+            this.txtgenre.Location = new System.Drawing.Point(74, 182);
+            this.txtgenre.Name = "txtgenre";
+            this.txtgenre.Size = new System.Drawing.Size(157, 23);
+            this.txtgenre.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Genre";
+            // 
+            // txtlength
+            // 
+            this.txtlength.Location = new System.Drawing.Point(74, 130);
+            this.txtlength.Name = "txtlength";
+            this.txtlength.Size = new System.Drawing.Size(157, 23);
+            this.txtlength.TabIndex = 5;
+            // 
+            // txtyear
+            // 
+            this.txtyear.Location = new System.Drawing.Point(74, 84);
+            this.txtyear.Name = "txtyear";
+            this.txtyear.Size = new System.Drawing.Size(157, 23);
+            this.txtyear.TabIndex = 4;
+            // 
+            // txttitle
+            // 
+            this.txttitle.Location = new System.Drawing.Point(74, 32);
+            this.txttitle.Name = "txttitle";
+            this.txttitle.Size = new System.Drawing.Size(157, 23);
+            this.txttitle.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Year";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Title";
+            // 
+            // btnew
+            // 
+            this.btnew.Location = new System.Drawing.Point(72, 329);
+            this.btnew.Name = "btnew";
+            this.btnew.Size = new System.Drawing.Size(73, 25);
+            this.btnew.TabIndex = 3;
+            this.btnew.Text = "New";
+            this.btnew.UseVisualStyleBackColor = true;
+            this.btnew.Click += new System.EventHandler(this.btnew_Click);
+            // 
+            // btadd
+            // 
+            this.btadd.Location = new System.Drawing.Point(204, 329);
+            this.btadd.Name = "btadd";
+            this.btadd.Size = new System.Drawing.Size(73, 25);
+            this.btadd.TabIndex = 4;
+            this.btadd.Text = "Add";
+            this.btadd.UseVisualStyleBackColor = true;
+            this.btadd.Click += new System.EventHandler(this.btadd_Click);
+            // 
+            // btupdate
+            // 
+            this.btupdate.Location = new System.Drawing.Point(72, 376);
+            this.btupdate.Name = "btupdate";
+            this.btupdate.Size = new System.Drawing.Size(73, 24);
+            this.btupdate.TabIndex = 5;
+            this.btupdate.Text = "Update";
+            this.btupdate.UseVisualStyleBackColor = true;
+            this.btupdate.Click += new System.EventHandler(this.btupdate_Click);
+            // 
+            // btdelete
+            // 
+            this.btdelete.Location = new System.Drawing.Point(204, 376);
+            this.btdelete.Name = "btdelete";
+            this.btdelete.Size = new System.Drawing.Size(73, 24);
+            this.btdelete.TabIndex = 6;
+            this.btdelete.Text = "Delete";
+            this.btdelete.UseVisualStyleBackColor = true;
+            this.btdelete.Click += new System.EventHandler(this.btdelete_Click);
+            // 
+            // movielist
+            // 
+            this.movielist.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.movielist.FullRowSelect = true;
+            this.movielist.HideSelection = false;
+            this.movielist.Location = new System.Drawing.Point(438, 24);
+            this.movielist.MultiSelect = false;
+            this.movielist.Name = "movielist";
+            this.movielist.Size = new System.Drawing.Size(406, 388);
+            this.movielist.TabIndex = 7;
+            this.movielist.UseCompatibleStateImageBehavior = false;
+            this.movielist.View = System.Windows.Forms.View.Details;
+            this.movielist.SelectedIndexChanged += new System.EventHandler(this.humanlist_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TITLE";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "YEAR";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "LENGTH";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "GENRE";
+            this.columnHeader4.Width = 100;
+            // 
+            // btexit
+            // 
+            this.btexit.Location = new System.Drawing.Point(72, 421);
+            this.btexit.Name = "btexit";
+            this.btexit.Size = new System.Drawing.Size(205, 27);
+            this.btexit.TabIndex = 8;
+            this.btexit.Text = "Exit";
+            this.btexit.UseVisualStyleBackColor = true;
+            this.btexit.Click += new System.EventHandler(this.btexit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(896, 473);
+            this.Controls.Add(this.btexit);
+            this.Controls.Add(this.movielist);
+            this.Controls.Add(this.btdelete);
+            this.Controls.Add(this.btupdate);
+            this.Controls.Add(this.btadd);
+            this.Controls.Add(this.btnew);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MovieForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtlength;
+        private System.Windows.Forms.TextBox txtyear;
+        private System.Windows.Forms.TextBox txttitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnew;
+        private System.Windows.Forms.Button btadd;
+        private System.Windows.Forms.Button btupdate;
+        private System.Windows.Forms.Button btdelete;
+        private System.Windows.Forms.ListView movielist;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox txtgenre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btexit;
+    }
+}
+
---
+++ Movie Management/SE140501/Form1.cs
@@ -0,0 +1,364 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SE140501
+{
+    public partial class Form1 : Form
+    {
+        int pos = -1;
+        //bool duplicate = false;
+        public Form1()
+        {
+            InitializeComponent();
+            FirstLoad();
+            LoadData();
+        }
+
+        private SqlConnection MakeConnection()
+        { 
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ConnectionString);
+            return conn;
+        }
+        private void LoadData()
+        {
+            try
+            {
+                SqlConnection conn = MakeConnection();
+                conn.Open();
+                SqlCommand cm = conn.CreateCommand();
+                cm.Connection = conn;
+                cm.CommandText = "select title,year,length,genre from Movies";
+                SqlDataReader dr = cm.ExecuteReader();
+                while (dr.Read())
+                {
+                    ListViewItem lvi = new ListViewItem(dr["title"].ToString());
+                    lvi.SubItems.Add(dr["year"].ToString());
+                    lvi.SubItems.Add(dr["length"].ToString());
+                    lvi.SubItems.Add(dr["genre"].ToString());
+                    movielist.Items.Add(lvi);
+                }
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Load data fail", "Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        private bool IsDuplicate(string title, int year)
+        {
+            bool a = false;
+            try
+            {
+                SqlConnection conn = MakeConnection();
+                conn.Open();
+                SqlCommand cm = conn.CreateCommand();
+                cm.Connection = conn;
+                cm.CommandText = "select title,year,length,genre from Movies where title = @t and year = @y";
+                cm.Parameters.Add("@t", SqlDbType.VarChar);
+                cm.Parameters.Add("@y", SqlDbType.Int);
+                cm.Parameters["@t"].Value = title;
+                cm.Parameters["@y"].Value = year;
+                SqlDataReader dr = cm.ExecuteReader();
+                if (dr.Read())
+                {
+                    a = true;
+                }
+                
+            }
+            catch
+            {
+
+            }
+            return a;
+        }
+        private bool ValidInput1()
+        {
+            string s;
+            //title
+            s = txttitle.Text.Trim();
+            if(s.Length == 0)
+            {
+                MessageBox.Show("Title is not empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txttitle.Focus();
+                return false;
+            }
+            //end title
+            //year
+            s = txtyear.Text.Trim();
+            if(s.Length == 0)
+            {
+                MessageBox.Show("Year is not empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtyear.Focus();
+                return false;
+            }
+            if(!Regex.IsMatch(s,"^[0-9]+$"))
+            {
+                MessageBox.Show("Year must be number only!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtyear.Focus();
+                return false;
+            }
+            if(int.Parse(s) < 1000 || int.Parse(s) > 3000)
+            {
+                MessageBox.Show("Year must be between 1000 - 3000!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtyear.Focus();
+                return false;
+            }
+            //end year
+            //length
+            s = txtlength.Text.Trim();
+            if (!Regex.IsMatch(s, "^[0-9]+$"))
+            {
+                MessageBox.Show("Length must be number > 0!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtlength.Focus();
+                return false;
+            }
+            if (int.Parse(s) <= 0)
+            {
+                MessageBox.Show("Length must be number > 0!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtlength.Focus();
+                return false;
+            }
+            // end length
+            //genre
+            s = txtgenre.Text.Trim();
+            if(s.Length == 0 || s.Length > 10)
+            {
+                MessageBox.Show("Genre'length min: 1, max: 10!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtgenre.Focus();
+                return false;
+            }
+            if(!Regex.IsMatch(s,"^[a-zA-z0-9 ]+$"))
+            {
+                MessageBox.Show("Genre must be letters only", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtgenre.Focus();
+                return false;
+            }
+            //end genre
+            //duplicate
+            if (IsDuplicate(txttitle.Text.Trim(), int.Parse(txtyear.Text.Trim())))
+            {
+                MessageBox.Show("This title and year were existed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txttitle.Focus();
+                return false;
+            }
+            //end duplicate
+            return true;
+        }
+        private bool ValidInput2()
+        {
+            string s;
+            //length
+            s = txtlength.Text.Trim();
+            if (!Regex.IsMatch(s, "^[0-9]+$"))
+            {
+                MessageBox.Show("Length must be number > 0!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtlength.Focus();
+                return false;
+            }
+            if (int.Parse(s) <= 0)
+            {
+                MessageBox.Show("Length must be number > 0!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtlength.Focus();
+                return false;
+            }
+            // end length
+            //genre
+            s = txtgenre.Text.Trim();
+            if (s.Length == 0 || s.Length > 10)
+            {
+                MessageBox.Show("Genre'length min: 1, max: 10!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtgenre.Focus();
+                return false;
+            }
+            //end genre
+            
+            return true;
+        }
+        private void btadd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ValidInput1())
+                {
+                    SqlConnection conn = MakeConnection();
+                    conn.Open();
+                    string title = txttitle.Text.Trim();
+                    string year = txtyear.Text.Trim();
+                    string length = txtlength.Text.Trim();
+                    string genre = txtgenre.Text.Trim();
+                    SqlCommand cm = new SqlCommand("insert into Movies(title,year,length,genre) values(@a,@b,@c,@d)", conn);
+                    cm.Parameters.Add("@a", SqlDbType.VarChar);
+                    cm.Parameters.Add("@b", SqlDbType.Int);
+                    cm.Parameters.Add("@c", SqlDbType.Int);
+                    cm.Parameters.Add("@d", SqlDbType.VarChar);
+                    cm.Parameters["@a"].Value = title;
+                    cm.Parameters["@b"].Value = year;
+                    cm.Parameters["@c"].Value = length;
+                    cm.Parameters["@d"].Value = genre;
+                    cm.ExecuteNonQuery();
+                    conn.Close();
+                    MessageBox.Show("Adding successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    movielist.Items.Clear();
+                    LoadData();
+                    FirstLoad();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Adding Fail!", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);   
+            }
+        }
+
+        private void humanlist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach(ListViewItem list in movielist.SelectedItems)
+            {
+                txttitle.Text = list.SubItems[0].Text.Trim();
+                txtyear.Text = list.SubItems[1].Text.Trim();
+                txtlength.Text = list.SubItems[2].Text.Trim();
+                txtgenre.Text = list.SubItems[3].Text.Trim();
+                pos = list.Index;
+            }
+            txttitle.Enabled = false;
+            txtyear.Enabled = false;
+            txtlength.Enabled = true;
+            txtgenre.Enabled = true;
+            btadd.Enabled = false;
+            btupdate.Enabled = true;
+            btdelete.Enabled = true;
+        }
+
+        private void btupdate_Click(object sender, EventArgs e)
+        {
+            if(pos < 0)
+            {
+                MessageBox.Show("Please choose a row for updating!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                try
+                {
+                    if (ValidInput2())
+                    {
+                        string title = txttitle.Text.Trim();
+                        string year = txtyear.Text.Trim();
+                        string length = txtlength.Text.Trim();
+                        string genre = txtgenre.Text.Trim();
+                        SqlConnection conn = MakeConnection();
+                        conn.Open();
+                        SqlCommand cm = new SqlCommand("update Movies set length = @a, genre = @b where title = @c and year = @d", conn);
+                        cm.Parameters.Add("@a", SqlDbType.Int);
+                        cm.Parameters.Add("@b", SqlDbType.VarChar);
+                        cm.Parameters.Add("@c", SqlDbType.VarChar);
+                        cm.Parameters.Add("@d", SqlDbType.Int);
+                        cm.Parameters["@a"].Value = length;
+                        cm.Parameters["@b"].Value = genre;
+                        cm.Parameters["@c"].Value = title;
+                        cm.Parameters["@d"].Value = year;
+                        cm.ExecuteNonQuery();
+                        conn.Close();
+                        MessageBox.Show("Updating Successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        movielist.Items.Clear();
+                        pos = -1;
+                        LoadData();
+                        FirstLoad();
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Updating Fail!", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btdelete_Click(object sender, EventArgs e)
+        {
+            if(pos < 0)
+            {
+                MessageBox.Show("Please choose a row for deleting!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                try
+                {
+                    var c = MessageBox.Show("Are you sure to delete?", "Confirm",
+                                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if(c == DialogResult.Yes)
+                    {
+                        string title = txttitle.Text.Trim();
+                        string year = txtyear.Text.Trim();
+                        SqlConnection conn = MakeConnection();
+                        conn.Open();
+                        SqlCommand cm = new SqlCommand("delete Movies where title = @a and year = @b", conn);
+                        cm.Parameters.Add("@a", SqlDbType.VarChar);
+                        cm.Parameters.Add("@b", SqlDbType.Int);
+                        cm.Parameters["@a"].Value = title;
+                        cm.Parameters["@b"].Value = year;
+                        cm.ExecuteNonQuery();
+                        conn.Close();
+                        txttitle.Text = "";
+                        txtyear.Text = "";
+                        txtlength.Text = "";
+                        txtgenre.Text = "";
+                        MessageBox.Show("Delete successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        movielist.Items.Clear();
+                        pos = -1;
+                        LoadData();
+                        FirstLoad();
+                    }
+                    
+                }
+                catch
+                {
+                    MessageBox.Show("Delete Fail!", "Fail", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnew_Click(object sender, EventArgs e)
+        {
+            txttitle.Enabled = true;
+            txtyear.Enabled = true;
+            txtlength.Enabled = true;
+            txtgenre.Enabled = true;
+            txttitle.Clear();
+            txtyear.Clear();
+            txtlength.Clear();
+            txtgenre.Clear();
+            btadd.Enabled = true;
+            btupdate.Enabled = false;
+            btdelete.Enabled = false;
+            pos = -1;
+        }
+        private void FirstLoad()
+        {
+            txttitle.Enabled = false;
+            txtyear.Enabled = false;
+            txtlength.Enabled = false;
+            txtgenre.Enabled = false;
+            btadd.Enabled = false;
+            btupdate.Enabled = true;
+            btdelete.Enabled = true;
+        }
+
+        private void btexit_Click(object sender, EventArgs e)
+        {
+            var c = MessageBox.Show("Are you sure to exit?", "Confirm",
+                                           MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (c == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
